8c0e9033a6151047f9e5871e9c80c1de9291aa36
thrwwy216 <thrwwy216@tutanota.com>
2020-08-19 08:14:29 -0500

    Added Configuration class and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# A basic Spring + Angular RESTful E-Commerce app 
+
+This project is based on a course by Chad Darby at https://www.udemy.com/course/full-stack-angular-spring-boot-tutorial/
---
+++ src/main/java/com/kwgdev/ecommerce/Config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.kwgdev.ecommerce.Config;
+
+import com.kwgdev.ecommerce.Entity.Product;
+import com.kwgdev.ecommerce.Entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.http.HttpMethod;
+
+/**
+ * created by kw on 8/19/2020 @ 7:55 AM
+ */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}

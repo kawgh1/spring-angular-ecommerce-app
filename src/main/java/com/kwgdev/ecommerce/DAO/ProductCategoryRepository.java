1efe9615b12bee063bab52c34730c4cb730ca5bc
thrwwy216 <thrwwy216@tutanota.com>
2020-08-21 18:46:59 -0500

    added findByCategoryId route method in JPA Product Repository

--- src/main/java/com/kwgdev/ecommerce/DAO/ProductCategoryRepository.java
+++ src/main/java/com/kwgdev/ecommerce/DAO/ProductCategoryRepository.java
@@ -3,10 +3,16 @@ package com.kwgdev.ecommerce.DAO;
 import com.kwgdev.ecommerce.Entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * created by kw on 8/19/2020 @ 7:29 AM
  */
+
+// without @CrossOrigin, the front-end will fail with a CORS error
+
+@CrossOrigin("http://localhost:4200") // allows our Spring REST to accept web browser calls from this http origin (angular frontend)
+
 // name of JSON entry = "productCategory", and path for the entry = "/product-category"
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- src/main/java/com/kwgdev/ecommerce/DAO/ProductRepository.java
+++ src/main/java/com/kwgdev/ecommerce/DAO/ProductRepository.java
@@ -1,10 +1,29 @@
 package com.kwgdev.ecommerce.DAO;
 
 import com.kwgdev.ecommerce.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * created by kw on 8/19/2020 @ 7:28 AM
  */
+// Origin is more than just the hostname
+
+    // Origin is protocol + hostname + port number
+
+    // Same Origin
+    // http://localhost:4200 == http://localhost:4200
+
+    // Different Origin
+    // http://localhost:4200 != http://localhost:8080
+
+// without @CrossOrigin, the front-end will fail with a CORS error
+
+@CrossOrigin("http://localhost:4200") // allows our Spring REST to accept web browser calls from this http origin (angular frontend)
 public interface ProductRepository extends JpaRepository <Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }

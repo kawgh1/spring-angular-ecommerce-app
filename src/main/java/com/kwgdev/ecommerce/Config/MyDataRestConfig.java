1e9bcecf08f2833cc34738ec057309f291f72da0
thrwwy216 <thrwwy216@tutanota.com>
2021-08-08 16:49:13 -0500

    added OrderRepository and config

--- src/main/java/com/kwgdev/ecommerce/Config/MyDataRestConfig.java
+++ src/main/java/com/kwgdev/ecommerce/Config/MyDataRestConfig.java
@@ -1,9 +1,6 @@
 package com.kwgdev.ecommerce.Config;
 
-import com.kwgdev.ecommerce.Entity.Country;
-import com.kwgdev.ecommerce.Entity.Product;
-import com.kwgdev.ecommerce.Entity.ProductCategory;
-import com.kwgdev.ecommerce.Entity.State;
+import com.kwgdev.ecommerce.Entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -46,6 +43,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
---
+++ src/main/java/com/kwgdev/ecommerce/DAO/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.kwgdev.ecommerce.DAO;
+
+import com.kwgdev.ecommerce.Entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * created by kw on 8/8/2021 @ 4:46 PM
+ */
+@RepositoryRestResource
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
+}

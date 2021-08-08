06598989c0680fb15ab56265f21b20f9f9f966db
thrwwy216 <thrwwy216@tutanota.com>
2021-08-08 10:23:13 -0500

    added logic to prevent duplicate customers in customer table when multiple orders are placed

--- src/main/java/com/kwgdev/ecommerce/DAO/CustomerRepository.java
+++ src/main/java/com/kwgdev/ecommerce/DAO/CustomerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * created by kw on 7/29/2021 @ 6:23 AM
  */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String theEmail);
 }

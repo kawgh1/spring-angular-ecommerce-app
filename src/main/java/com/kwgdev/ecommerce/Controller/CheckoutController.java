01356a93c7e4c7bf0da0553d0c24baf9616f68f1
thrwwy216 <thrwwy216@tutanota.com>
2021-08-10 06:49:24 -0500

    controller

--- src/main/java/com/kwgdev/ecommerce/Controller/CheckoutController.java
+++ src/main/java/com/kwgdev/ecommerce/Controller/CheckoutController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
  * created by kw on 7/29/2021 @ 6:36 AM
  */
 
-// @CrossOrigin("http://localhost:4200") // - configured in MyDataRestConfig under cors.addMapping(...).allowedOrigins("...")
+@CrossOrigin("https://spring-angular-ecommerce-front.herokuapp.com") // - configured in MyDataRestConfig under cors.addMapping(...).allowedOrigins("...")
 @RestController
-@RequestMapping("api/checkout")
+@RequestMapping("/api/checkout")
 public class CheckoutController {
 
     private CheckoutService checkoutService;
